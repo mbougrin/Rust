0090c2b13778384b35bc19b5bc8f52d1250029ca
mbougrin <mbougrin@student.42.fr>
2022-07-29 12:45:50 +0000

    modifcation ft_time_server adding time write at user logged at server

--- ft_time_server/src/main.rs
+++ ft_time_server/src/main.rs
@@ -3,18 +3,27 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::env;
 use std::thread;
-//use std::time::{SystemTime, UNIX_EPOCH};
+use std::time::Duration;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+fn print_time() {
+    println!("{}", SystemTime::now()
+             .duration_since(UNIX_EPOCH)
+             .unwrap().as_millis());
+
+}
 
 fn handle_connection(mut stream: TcpStream) {
-//    let now = SystemTime::now();
-
     loop {
-        /*let since_the_epoch = now
+        let since_the_epoch = SystemTime::now()
             .duration_since(UNIX_EPOCH)
-            .expect("Time went backwards");
-        */
-        stream.write(b"HelloWorld!").unwrap();
+            .unwrap().as_millis();
+        let mut s = since_the_epoch.to_string();
+        s.push_str("\n");
+        stream.write(s.as_bytes()).unwrap();
         stream.flush().unwrap();
+        let ms = Duration::from_millis(1000);
+        thread::sleep(ms);
     }
 }
 
@@ -28,7 +37,6 @@ fn run_server(server_ip_port: &String) {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("New client connection etablished!");
         thread::spawn(|| {
             handle_connection(stream)
         });
@@ -39,6 +47,7 @@ fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
     match args.len() {
         3 => {
+            print_time();
             let mut server_ip_port = String::with_capacity(128);
             server_ip_port.push_str(&args[1]);
             server_ip_port.push_str(":");
